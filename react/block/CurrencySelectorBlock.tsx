b16f1e8906f20dba59534a2ca274cb0ea67b4723
Filadelfo <filadelfo.braz@gmail.com>
2022-01-05 15:35:48 +0100

    Feat: Make curency selector label editable via site editor

--- react/block/CurrencySelectorBlock.tsx
+++ react/block/CurrencySelectorBlock.tsx
@@ -1,18 +1,55 @@
 import React from 'react'
+import { IOMessageWithMarkers } from 'vtex.native-types'
+import { defineMessages } from 'react-intl'
 
 import { useCurrencySelector } from './hooks/useCurrencySelector'
 
-const CurrencySelectorBlock = () => {
+const messages = defineMessages({
+  title: { id: 'admin/currency-selector.title' },
+  default: {
+    id: 'store/currency-selector.label-format.default',
+  },
+  description: {
+    id: 'store/currency-selector.label-format.description',
+  },
+})
+
+interface Props {
+  labelFormat: string
+}
+
+const CurrencySelectorBlock = ({
+  labelFormat = messages.default.id,
+}: Props) => {
   const { currentSalesChannel, salesChannelList } = useCurrencySelector()
 
   // eslint-disable-next-line no-console
-  console.log({ currentSalesChannel, salesChannelList })
+  console.log({ currentSalesChannel, salesChannelList, labelFormat })
+
+  if (!currentSalesChannel) {
+    return null
+  }
+
+  const { CurrencyCode, CurrencySymbol, customLabel } = currentSalesChannel
 
   return (
-    <div>
-      <h1>CurrencySelectorBlock</h1>
-    </div>
+    <span>
+      <IOMessageWithMarkers
+        message={labelFormat}
+        markers={[]}
+        handleBase="currency-label"
+        values={{
+          CurrencySymbol: <span key="CurrencySymbol">{CurrencySymbol}</span>,
+          CurrencyCode: <span key="CurrencyCode">{CurrencyCode}</span>,
+          customLabel: <span key="customLabel">{customLabel}</span>,
+        }}
+      />
+    </span>
   )
+}
+
+CurrencySelectorBlock.schema = {
+  title: messages.title.id,
 }
 
 export default CurrencySelectorBlock
