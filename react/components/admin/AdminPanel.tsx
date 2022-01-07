19e32613cb27dec1c5f22062a62d3a832863db0c
Filadelfo <filadelfo.braz@gmail.com>
2022-01-07 16:14:42 +0100

    Feat: Replace fake tenant info using graphql to tenant app

--- react/components/admin/AdminPanel.tsx
+++ react/components/admin/AdminPanel.tsx
@@ -1,36 +1,48 @@
 import type { FC } from 'react'
 import React, { useEffect, useState } from 'react'
 import { Layout, PageBlock, PageHeader, Toggle } from 'vtex.styleguide'
+import { useQuery } from 'react-apollo'
+import type { Tenant } from 'vtex.tenant-graphql'
 
 import { BindingInfo } from './BindingInfo'
-import { tenantInfo } from './tenants'
+import TENANT_INFO from '../../graphql/tenantInfo.gql'
 
 const AdminPanel: FC = () => {
-  const [tenant] = useState(tenantInfo)
   const [settings, setSettings] = useState<any[]>([])
   const [isBindingBounded, setIsBindingBounded] = useState(true)
+
+  const {
+    data: tenantData,
+    loading,
+    error,
+  } = useQuery<{ tenantInfo: Tenant }>(TENANT_INFO)
+
+  // eslint-disable-next-line no-console
+  console.log({ loading, error })
 
   const handleChangeBindingBounded = () => {
     setIsBindingBounded(!isBindingBounded)
   }
 
   useEffect(() => {
-    const config = tenant.bindings
-      .filter(binding => binding.targetProduct === 'vtex-storefront')
-      .map(({ id, canonicalBaseAddress, extraContext }) => {
-        return {
-          bindingId: id,
-          canonicalBaseAddress,
-          salesChannelInfo: [
-            {
-              salesChannel: extraContext?.portal?.salesChannel,
-            },
-          ],
-        }
-      })
+    if (tenantData) {
+      const config = tenantData.tenantInfo.bindings
+        .filter(binding => binding.targetProduct === 'vtex-storefront')
+        .map(({ id, canonicalBaseAddress, extraContext }) => {
+          return {
+            bindingId: id,
+            canonicalBaseAddress,
+            salesChannelInfo: [
+              {
+                salesChannel: extraContext?.portal?.salesChannel,
+              },
+            ],
+          }
+        })
 
-    setSettings(config)
-  }, [])
+      setSettings(config)
+    }
+  }, [tenantData])
 
   return (
     <Layout pageHeader={<PageHeader title="Currency Selector"></PageHeader>}>
