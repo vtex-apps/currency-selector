80bf56b82540498a553f0940855b8c5531500e20
Asem Qaffaf <51743718+asemqaffaf@users.noreply.github.com>
2022-01-10 14:09:40 +0200

    feat: implement updateSalesChannelInfo mutation and SCSwitcherConfiguration query

--- node/resolvers/salesChannel.ts
+++ node/resolvers/salesChannel.ts
@@ -5,6 +5,8 @@ export const SalesChannel = {
   currencySymbol: (root: SalesChannelResponse) => root.CurrencySymbol,
   isActive: (root: SalesChannelResponse) => root.IsActive,
 }
+const BUCKET = 'currency_selector'
+const CONFIG_PATH = 'configs'
 
 const salesChannel = async (_root: unknown, _: unknown, ctx: Context) => {
   const {
@@ -16,6 +18,45 @@ const salesChannel = async (_root: unknown, _: unknown, ctx: Context) => {
   return salesChannelResponse
 }
 
+const updateSalesChannelInfo = async (
+  _root: unknown,
+  { bindingId, salesChannelInfo }: CurrencySelectorConfig,
+  ctx: Context
+) => {
+  const { clients } = ctx
+  const args = { bindingId, salesChannelInfo }
+  const { vbase } = clients
+
+  await vbase.saveJSON(BUCKET, CONFIG_PATH, {
+    ...args,
+  })
+
+  return args
+}
+
+const SCSwitcherConfiguration = async (
+  _root: unknown,
+  _: unknown,
+  ctx: Context
+) => {
+  const { clients } = ctx
+  const { vbase } = clients
+
+  const savedSalesChannelInfo =
+    await vbase.getJSON<CurrencySelectorConfig | null>(
+      BUCKET,
+      CONFIG_PATH,
+      true
+    )
+
+  return savedSalesChannelInfo
+}
+
 export const queries = {
   salesChannel,
+  SCSwitcherConfiguration,
+}
+
+export const mutations = {
+  updateSalesChannelInfo,
 }
