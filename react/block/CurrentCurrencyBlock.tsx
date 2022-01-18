496f4f539813f7605089576cab85c78d548f6436
Filadelfo <filadelfo.braz@gmail.com>
2022-01-18 14:47:59 +0100

    Feat: Add Current Currency block

--- react/block/CurrentCurrencyBlock.tsx
+++ react/block/CurrentCurrencyBlock.tsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import { defineMessages } from 'react-intl'
+import { useCssHandles } from 'vtex.css-handles'
+
+import { useCurrencySelector } from './hooks/useCurrencySelector'
+import Spinner from './Spinner'
+import CustomLabel from './CustomLabel'
 
 const messages = defineMessages({
   title: { id: 'admin/current-currency.title' },
+  default: {
+    id: 'store/currency-selector.label-format.default',
+  },
 })
 
-const CurrentCurrencyBlock = () => {
-  return <div>Current currency</div>
+const CSS_HANDLES = ['container'] as const
+
+interface Props {
+  labelFormat: string
+}
+
+const CurrentCurrencyBlock = ({ labelFormat = messages.default.id }: Props) => {
+  const { currentSalesChannel, isLoading, hasError } = useCurrencySelector()
+
+  const { handles } = useCssHandles(CSS_HANDLES)
+
+  return hasError ? null : (
+    <div
+      className={`flex items-center justify-center relative h-100 pointer ${handles.container}`}
+    >
+      {isLoading || !currentSalesChannel ? (
+        <Spinner />
+      ) : (
+        <CustomLabel {...currentSalesChannel} labelFormat={labelFormat} />
+      )}
+    </div>
+  )
 }
 
 CurrentCurrencyBlock.schema = {
